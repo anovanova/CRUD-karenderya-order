d8e2c0d6240aa429d1942a6ce6278e2410ae3a97
anovanova <acederamichael07@gmail.com>
2023-09-05 16:12:01 +0800

    added nav buttons functions

--- index.php
+++ index.php
@@ -33,5 +33,6 @@
     </section>
     <script src="js/jquery-3.6.0.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/button-functions.js"></script>
 </body>
 </html>
--- js/button-functions.js
+++ js/button-functions.js
@@ -1,3 +1,15 @@
-$('#createFoodName').on('click', () => {
-
+$('#homeBtn').on('click', () => {
+    $('#createFoodItemContainer').attr('hidden', true);
+    $('#orderHistoryContainer').attr('hidden', true);
+    $('#orderFoodContainer').attr('hidden', false);
+})
+$('#orderHistoryBtn').on('click', () => {
+    $('#createFoodItemContainer').attr('hidden', true);
+    $('#orderHistoryContainer').attr('hidden', false);
+    $('#orderFoodContainer').attr('hidden', true);
+})
+$('#createFoodItemBtn').on('click', () => {
+    $('#createFoodItemContainer').attr('hidden', false);
+    $('#orderHistoryContainer').attr('hidden', true);
+    $('#orderFoodContainer').attr('hidden', true);
 })
