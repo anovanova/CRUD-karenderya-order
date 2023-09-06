1ecbe40fc70719afe7e363ffc0d2eb96e322bd77
Michael Acedera <44719933+anovanova@users.noreply.github.com>
2023-09-06 11:01:04 +0800

    Add files via upload

---
+++ karenderya (1).sql
@@ -0,0 +1,85 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 06, 2023 at 04:58 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `karenderya`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food_items_tbl`
+--
+
+CREATE TABLE `food_items_tbl` (
+  `id` int(11) NOT NULL,
+  `food_name` varchar(50) NOT NULL,
+  `food_quantity` int(100) NOT NULL,
+  `food_price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food_orders_tbl`
+--
+
+CREATE TABLE `food_orders_tbl` (
+  `id` int(11) NOT NULL,
+  `food_name` varchar(50) NOT NULL,
+  `food_quantity` int(100) NOT NULL,
+  `order_price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `food_items_tbl`
+--
+ALTER TABLE `food_items_tbl`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `food_orders_tbl`
+--
+ALTER TABLE `food_orders_tbl`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `food_items_tbl`
+--
+ALTER TABLE `food_items_tbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `food_orders_tbl`
+--
+ALTER TABLE `food_orders_tbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
